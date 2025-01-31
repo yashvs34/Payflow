fb492f3a281a03fb8861d5b526d8e2012e4e08fc
Yash <yashvardhansingh232@gmail.com>
2025-01-31 23:47:30 +0530

    m

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -3,7 +3,7 @@ export function SearchBar ()
 {
     return (
         <div>
-            <input type="text" className = "w-[600px] mx-10 p-1 font-thin font-semibold text-sm border-[2px] border-gray-400 rounded-md" />
+            <input type="text" className = "w-[600px] mx-10 p-1 font-thin text-sm border-[2px] border-gray-400 rounded-md" />
         </div>
     )
 }

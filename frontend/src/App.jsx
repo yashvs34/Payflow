722f5364433737cc7173ce42dd917861d7b2b000
yashvs34 <130848758+yashvs34@users.noreply.github.com>
2025-05-24 23:03:42 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes, BrowserRouter } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 import {Signin} from './pages/Signin' 
 import {Signup} from './pages/Signup' 
 import {Dashboard} from './pages/Dashboard' 
@@ -8,15 +8,13 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path = "/" element = {<Signin/>} ></Route>
-          <Route path = "/signin" element = {<Signin/>} ></Route>
-          <Route path = "/signup" element = {<Signup/>} ></Route>
-          <Route path = "/dashboard" element = {<Dashboard/>} ></Route>
-          <Route path = "/sendmoney" element = {<SendMoney/>} ></Route>
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path = "/" element = {<Signin/>} ></Route>
+        <Route path = "/signin" element = {<Signin/>} ></Route>
+        <Route path = "/signup" element = {<Signup/>} ></Route>
+        <Route path = "/dashboard" element = {<Dashboard/>} ></Route>
+        <Route path = "/sendmoney" element = {<SendMoney/>} ></Route>
+      </Routes>
     </>
   )
 }

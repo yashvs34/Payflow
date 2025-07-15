3aa37b3b55c763e218d3b5bba2573b4615ad37c4
yashvs34 <130848758+yashvs34@users.noreply.github.com>
2025-07-15 16:28:38 +0530

    Added BrowserRouter tag in correct place

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import {Signin} from './pages/Signin' 
 import {Signup} from './pages/Signup' 
 import {Dashboard} from './pages/Dashboard' 
@@ -8,13 +8,15 @@ function App() {
 
   return (
     <>
-      <Routes>
-        <Route path = "/" element = {<Signin/>} ></Route>
-        <Route path = "/signin" element = {<Signin/>} ></Route>
-        <Route path = "/signup" element = {<Signup/>} ></Route>
-        <Route path = "/dashboard" element = {<Dashboard/>} ></Route>
-        <Route path = "/sendmoney" element = {<SendMoney/>} ></Route>
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path = "/" element = {<Signin/>} ></Route>
+          <Route path = "/signin" element = {<Signin/>} ></Route>
+          <Route path = "/signup" element = {<Signup/>} ></Route>
+          <Route path = "/dashboard" element = {<Dashboard/>} ></Route>
+          <Route path = "/sendmoney" element = {<SendMoney/>} ></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
